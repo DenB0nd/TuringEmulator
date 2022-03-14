a0ee626f714bfb73476b4dda60f8fe2c4d188372
Admin <dbondarenko066@mail.ru>
2022-03-15 02:19:02 +0300

    DarkTheme.xaml + keep designing

--- TuringEmulator/Program.cs
+++ TuringEmulator/Program.cs
@@ -7,22 +7,15 @@ namespace TuringEmulator
         static void Main(string[] args)
         {
             TuringMachine machine = new();
-            machine.Alphabet = " 1";
+            machine.Alphabet = " 01";
             machine.State = 0;
-            machine.Tape = new InfiniteTape("111 111", 0);
+            machine.Tape = new InfiniteTape("0101010", 0);
             TransitionFunctionsTable table = new(
                 new[]
                 {
-                    new TransitionFunction(0, ' ', 0, ' ', Directions.Right),
-                    new TransitionFunction(0, '1', 1, '1', Directions.Right),
-                    new TransitionFunction(1, ' ', 2, '1', Directions.Right),
-                    new TransitionFunction(1, '1', 1, '1', Directions.Right),
-                    new TransitionFunction(2, ' ', 3, ' ', Directions.Left),
-                    new TransitionFunction(2, '1', 2, '1', Directions.Right),
-                    new TransitionFunction(3, ' ', 3, ' ', Directions.Left),
-                    new TransitionFunction(3, '1', 4, ' ', Directions.Left),
-                    new TransitionFunction(4, ' ', TuringMachine.HALT, ' ', Directions.None),
-                    new TransitionFunction(4, '1', 4, '1', Directions.Left)
+                    new TransitionFunction(0, '0', 0, '1', Directions.Right),
+                    new TransitionFunction(0, '1', 0, '0', Directions.Right),
+                    new TransitionFunction(0, ' ', TuringMachine.HALT, ' ', Directions.None)
                 });
             machine.Table = table;
 
