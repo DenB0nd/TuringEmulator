b461fef95ed6be9eca68092da9ca6cabaa3b3758
Admin <dbondarenko066@mail.ru>
2022-03-16 00:34:09 +0300

    redesign :/

--- TuringMachineAppWPF/MainWindow.xaml.cs
+++ TuringMachineAppWPF/MainWindow.xaml.cs
@@ -20,7 +20,6 @@ namespace TuringMachineAppWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        
         public MainWindow()
         {
             InitializeComponent();
